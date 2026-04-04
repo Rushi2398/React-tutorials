e30d7c344155a7deda8f048a1d5d0f86d8bdb5ff
Rushikesh Jalvi <rushikeshjalvi23@gmail.com>
2026-04-04 15:56:47 +0530

    React Basics

---
+++ Components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return(
+    <div className='card'>
+        <h1>Rushikesh Jalvi</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, similique.</p>
+    </div>
+          )
+}
+
+export default Card
