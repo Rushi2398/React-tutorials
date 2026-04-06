7b457111f892c48cd63537811e51a3bf0ae8cc03
Rushikesh Jalvi <rushikeshjalvi23@gmail.com>
2026-04-06 22:06:33 +0530

    Tailwind setup and Small UI proj

---
+++ Checkpoint-project/src/components/Section1/Arrow.jsx
@@ -0,0 +1,11 @@
+import { MoveUpRight } from 'lucide-react'
+
+const Arrow = () => {
+  return (
+    <div>
+        <MoveUpRight size={80} strokeWidth={3}/>
+    </div>
+  )
+}
+
+export default Arrow
---
+++ Checkpoint-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,10 @@
+const HeroText = () => {
+  return (
+    <div className='p-6'>
+        <h3 className='mb-7 text-6xl leading-[1.1] font-bold'>Prospective <br/><span>customer </span><br/>segmentation</h3>
+        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum temporibus, sint hic praesentium voluptatem repellat dicta accusantium perferendis architecto ullam.</p>
+    </div>
+  )
+}
+
+export default HeroText
